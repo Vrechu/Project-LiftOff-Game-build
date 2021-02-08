3cfc9bb09c4e1dfa5b88c6f8c0a8ffa84ce42993
Freek Michielsen <480709@student.saxion.nl>
2021-02-08 17:32:46 +0100

    Add vec2 class

---
+++ GXPEngine/GXPEngine/Vec2.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using GXPEngine;
+using GXPEngine.Core;
+
+//--------------------------------------------------------------------------------------------------
+//                                         VECTOR STRUCT
+//--------------------------------------------------------------------------------------------------
+// my finished vector struct
+public struct Vec2
+{
+	//vector's coördinates
+	public float x;
+	public float y;
+
+	public Vec2(float pX = 0, float pY = 0)
+	{
+		x = pX;
+		y = pY;
+	}
+
+	//vector addition
+	public static Vec2 operator +(Vec2 left, Vec2 right)
+	{
+		return new Vec2(left.x + right.x, left.y + right.y);
+	}
+
+	//vector subtracion
+	public static Vec2 operator -(Vec2 left, Vec2 right)
+	{
+		return new Vec2(left.x - right.x, left.y - right.y);
+	}
+
+	//vector scaling
+	public static Vec2 operator *(Vec2 left, float right)
+	{
+		return new Vec2(left.x * right, left.y * right);
+	}
+	public static Vec2 operator *(float left, Vec2 right)
+	{
+		return new Vec2(left * right.x, left * right.y);
+	}
+
+	//vector division
+	public static Vec2 operator /(Vec2 left, float right)
+	{
+		return new Vec2(left.x / right, left.y / right);
+	}
+
+	//calculates the lenght of the vector
+	public float Length()
+	{
+		return Mathf.Sqrt((this.x * this.x) + (this.y * this.y));
+	}
+
+	//scales the vector length to 1 without changing the direction
+	public void Normalize()
+	{
+		if (Length() == 0)
+		{
+			this *= 0;
+		}
+		if (Length() != 0)
+		{
+			this *= 1 / Length();
+		}
+	}
+
+	//returns a new vector with length 1 in the same direction od the original 
+	public Vec2 Normalized()
+	{
+		if (Length() != 0)
+		{
+			Vec2 normalizedVec = this;
+			normalizedVec.Normalize();
+			return normalizedVec;
+		}
+		else
+		{
+			return new Vec2(0, 0);
+		}
+	}
+
+	public override string ToString()
+	{
+		return String.Format("({0},{1})", x, y);
+	}
+
+	//changes the x and y coördinates of the vector
+	public void SetXY(float _x, float _y)
+	{
+		this.x = _x;
+		this.y = _y;
+	}
+
+	//converts angles from degrees to radians or from radians to degrees
+	public float Deg2Rad(float _degree)
+	{
+		return (_degree * (Mathf.PI / 180));
+	}
+	public float Rad2Deg(float _radian)
+	{
+		return (_radian * (180 / Mathf.PI));
+	}
+
+	//returns a vector with length 1 and the specified angle
+	public Vec2 GetUnitVectorDeg(float degree)
+	{
+		return new Vec2(Mathf.Cos(Deg2Rad(degree)), Mathf.Sin(Deg2Rad(degree)));
+	}
+	public Vec2 GetUnitVectorRad(float radian)
+	{
+		return new Vec2(Mathf.Cos(radian), Mathf.Sin(radian));
+	}
+
+	//returns a vector with lenght 1 and a random angle
+	public Vec2 RandomUnitVector()
+	{
+		float randomAngle;
+		var rand = new Random();
+		var randomNumber = (float)rand.NextDouble() * 2;
+		if (randomNumber > 1)
+		{
+			randomNumber = -1 + (randomNumber - 1);
+		}
+		randomAngle = Mathf.PI * randomNumber;
+		return GetUnitVectorRad(randomAngle);
+	}
+
+	//sets the vector to the specified angle
+	public void SetAngleRadians(float radian)
+	{
+		var length = Length();
+		SetXY(Mathf.Cos(radian) * length, Mathf.Sin(radian) * length);
+	}
+	public void SetAngleDegrees(float degree)
+	{
+		var length = Length();
+		SetXY(Mathf.Cos(Deg2Rad(degree)) * length, Mathf.Sin(Deg2Rad(degree)) * length);
+	}
+
+	//returns the angle of the vector
+	public float GetAngleDegrees()
+	{
+		return Rad2Deg(Mathf.Atan2(this.y, this.x));
+	}
+	public float GetAngleRadians()
+	{
+		return Mathf.Atan2(this.y, this.x);
+	}
+
+	//rotates the vector over the specified angle
+	public void RotateDegrees(float degree)
+	{
+		float angleSine = Mathf.Sin(Deg2Rad(degree));
+		float angleCosine = Mathf.Cos(Deg2Rad(degree));
+		SetXY(this.x * angleCosine - this.y * angleSine, this.x * angleSine + this.y * angleCosine);
+	}
+	public void RotateRadians(float radian)
+	{
+		float angleSine = Mathf.Sin(radian);
+		float angleCosine = Mathf.Cos(radian);
+		SetXY(this.x * angleCosine - this.y * angleSine, this.x * angleSine + this.y * angleCosine);
+	}
+
+	//rotates the vector around the specified point over the specified angle
+	public void RotateAroundDegrees(Vec2 point, float degree)
+	{
+		this -= point;
+		this.RotateDegrees(degree);
+		this += point;
+	}
+	public void RotateAroundRadians(Vec2 point, float radian)
+	{
+		this -= point;
+		this.RotateRadians(radian);
+		this += point;
+	}
+
+	//returns the dot product of two vectors
+	public float Dot(Vec2 other)
+	{
+		return this.x * other.x + this.y * other.y;
+	}
+
+	//returns a normal with length 1 of the vector 
+	public Vec2 UnitNormal()
+	{
+		Vec2 normal = new Vec2(this.y * -1, this.x);
+		normal.Normalize();
+		return normal;
+	}
+
+	//changes the vector angle to the reflected version of the angle compared to a different vector
+	//the "bounciness" of the vector is specified with the COR from 0 to 1
+	public void Reflect(Vec2 other, float COR)
+	{
+		this -= (1 + COR) * (this.Dot(other.UnitNormal())) * other.UnitNormal();
+	}
+}
---
+++ GXPEngine/GXPEngine/Vec2UnitTest.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Threading;
+using GXPEngine;
+//--------------------------------------------------------------------------------------------------
+//                                         VECTOR UNIT TESTING CLASS
+//--------------------------------------------------------------------------------------------------
+// class containing all tests for the vector struct
+class Vec2UnitTest : GameObject
+{
+	//test vectors
+	Vec2 testVec1 = new Vec2(2, 4);
+	Vec2 testVec2 = new Vec2(5, 10);
+	Vec2 testVec3 = new Vec2(3, 6);
+	//test angles
+	float testDeg1 = new float();
+	float testRad1 = new float();
+
+	public Vec2UnitTest()
+	{
+	}
+
+	void Update()
+	{
+		//input for doing tests
+		if (Input.GetKeyUp(Key.V))
+		{
+			UnitTests();
+		}
+	}
+
+	private void UnitTests()
+	{
+		// all vector method tests
+
+		//testvec 1: x = 2, y = 4
+		//testvec 2: x = 5, y = 10
+		//testvec 3: x = 3, y = 6
+		//testDeg 1: 90 degrees
+		//testRad 1: 0,25 PI radians
+
+		FloatIsEqualTest();
+		ResetTestValues();
+		SestSetXY();
+		AddTest();
+		SubtractTest();
+		ScaleTest();
+		LengthTest();
+		NormalizeTest();
+		NormalizedTest();
+		Deg2RadTest();
+		Rad2DegTest();
+		UnitVectorDegTest();
+		UnitVectorRadTest();
+		RandomUnitVectorTest();
+		GetAngleDegreesTest();
+		GetAnglesRadiansTest();
+		SetAngleRadiansTest();
+		SetAngleDegreesTest();
+		RotateDegreesTest();
+		RotateRadiansTest();
+		RotateAroundDegreesTest();
+		RotateAroundRadiansTest();
+		DotTest();
+		UnitNormalTest();
+		ReflectTest();
+	}
+
+	private void ResetTestValues()
+	{
+		//resets the test vectors and angles for later use
+		testVec1.SetXY(2, 4);
+		testVec2.SetXY(5, 10);
+		testVec3.SetXY(3, 6);
+
+		testDeg1 = 90;
+		testRad1 = 0.25f * Mathf.PI;
+		Console.WriteLine("-");
+	}
+
+	bool FloatIsEqual(float left, float right)
+	{
+		//replacement for "==" since some methods don't return perfectly accurate values 
+		return (Mathf.Abs(left - right) < 0.00001);
+	}
+
+	void FloatIsEqualTest()
+	{
+		Console.WriteLine("float is equal ok? :"
+			+ FloatIsEqual(2.000001f, 2.000002f));
+	}
+
+	private void SestSetXY()
+	{
+		testVec1.SetXY(5, 7);
+		testVec2.SetXY(2, 3);
+		testVec3.SetXY(10, 20);
+		Console.WriteLine("SetXY ok? :"
+			+ (testVec1.x == 5 && testVec1.y == 7
+			&& testVec2.x == 2 && testVec2.y == 3
+			&& testVec3.x == 10 && testVec3.y == 20));
+		ResetTestValues();
+	}
+
+	private void AddTest()
+	{
+		testVec3 = testVec1 + testVec2;
+		Console.WriteLine("Addition ok? :"
+			+ (testVec1.x == 2 && testVec1.y == 4
+			&& testVec2.x == 5 && testVec2.y == 10
+			&& testVec3.x == 7 && testVec3.y == 14));
+		ResetTestValues();
+	}
+
+	private void SubtractTest()
+	{
+		testVec3 = testVec2 - testVec1;
+		Console.WriteLine("Subtraction ok? :"
+			+ (testVec1.x == 2 && testVec1.y == 4
+			&& testVec2.x == 5 && testVec2.y == 10
+			&& testVec3.x == 3 && testVec3.y == 6));
+		ResetTestValues();
+	}
+
+	private void ScaleTest()
+	{
+		testVec2 = testVec1 * 2;
+		testVec3 = 3 * testVec1;
+		Console.WriteLine("Scaling ok? :"
+			+ (testVec1.x == 2 && testVec1.y == 4
+			&& testVec2.x == 4 && testVec2.y == 8
+			&& testVec3.x == 6 && testVec3.y == 12));
+		ResetTestValues();
+	}
+
+	private void LengthTest()
+	{
+		Console.WriteLine("Calculate length ok? :"
+			+ (testVec1.Length() == Mathf.Sqrt(20)
+			&& testVec2.Length() == Mathf.Sqrt(125)));
+		ResetTestValues();
+	}
+
+	private void NormalizeTest()
+	{
+		testVec1.Normalize();
+		testVec2.SetXY(0, 0);
+		testVec2.Normalize();
+		Console.WriteLine("Normalize ok? :"
+			+ (testVec1.Length() == 1
+			&& testVec2.Length() == 0));
+		ResetTestValues();
+	}
+
+	private void NormalizedTest()
+	{
+		testVec2.SetXY(0, 0);
+		Console.WriteLine("Normalized new Vec ok? :"
+			+ (testVec1.Normalized().Length() == 1
+			&& testVec2.Normalized().Length() == 0));
+		ResetTestValues();
+	}
+
+	private void Deg2RadTest()
+	{
+		Console.WriteLine("Degrees to Radians ok? :"
+		+ FloatIsEqual(testVec1.Deg2Rad(testDeg1), 0.5f * Mathf.PI));
+		ResetTestValues();
+	}
+
+	private void Rad2DegTest()
+	{
+		Console.WriteLine("Radians to Degrees ok? :"
+		+ FloatIsEqual(testVec1.Rad2Deg(testRad1), 45));
+		ResetTestValues();
+	}
+
+	private void UnitVectorDegTest()
+	{
+		Console.WriteLine("UnitVector Degrees ok? :"
+			+ (testVec1.GetUnitVectorDeg(testDeg1).x == Mathf.Cos(testVec1.Deg2Rad(90))
+			&& testVec1.GetUnitVectorDeg(testDeg1).y == Mathf.Sin(testVec1.Deg2Rad(90))
+			&& FloatIsEqual(Mathf.Sqrt(Mathf.Pow(testVec1.GetUnitVectorDeg(testVec1.Deg2Rad(testDeg1)).x, 2) +
+			Mathf.Pow(testVec1.GetUnitVectorDeg(testVec1.Deg2Rad(testDeg1)).y, 2)), 1f)));
+		ResetTestValues();
+	}
+
+	private void UnitVectorRadTest()
+	{
+		Console.WriteLine("UnitVector Radians ok? :"
+			+ (testVec1.GetUnitVectorRad(testRad1).x == Mathf.Cos(0.25f * Mathf.PI)
+			&& testVec1.GetUnitVectorRad(testRad1).y == Mathf.Sin(0.25f * Mathf.PI)
+			&& FloatIsEqual(Mathf.Sqrt(Mathf.Pow(testVec1.GetUnitVectorRad(testRad1).x, 2) +
+			Mathf.Pow(testVec1.GetUnitVectorRad(testRad1).y, 2)), 1f)));
+		ResetTestValues();
+	}
+
+	private void RandomUnitVectorTest()
+	{
+		Console.WriteLine("Random UnitVector ok? :"
+			+ (FloatIsEqual(testVec1.RandomUnitVector().Length(), 1f)));
+		ResetTestValues();
+	}
+
+	private void GetAngleDegreesTest()
+	{
+		testVec1.SetXY(5, 5);
+		testVec2.SetXY(-5, -5);
+		Console.WriteLine("Get Angle Degrees ok? :"
+			+ (testVec1.GetAngleDegrees() == 45
+			&& testVec2.GetAngleDegrees() == -135));
+		ResetTestValues();
+	}
+
+	private void GetAnglesRadiansTest()
+	{
+		testVec1.SetXY(5, 5);
+		testVec2.SetXY(-5, -5);
+		Console.WriteLine("Get Angle Radians ok? :"
+			+ (testVec1.GetAngleRadians() == 0.25 * Mathf.PI
+			&& FloatIsEqual(testVec2.GetAngleRadians(), -0.75f * Mathf.PI)));
+		ResetTestValues();
+	}
+
+	private void SetAngleRadiansTest()
+	{
+		testVec1.SetAngleRadians(testRad1);
+		testVec2.SetAngleRadians(-0.25f * Mathf.PI);
+		Console.WriteLine("Set Angle Radians ok? :"
+			+ (testVec1.Length() == Mathf.Sqrt(20)
+			&& testVec1.GetAngleRadians() == testRad1
+			&& FloatIsEqual(testVec2.GetAngleRadians(), -0.25f * Mathf.PI)));
+		ResetTestValues();
+	}
+
+	private void SetAngleDegreesTest()
+	{
+		testVec1.SetAngleDegrees(testDeg1);
+		testVec2.SetAngleDegrees(-45);
+		Console.WriteLine("Set Angle Degrees ok? :"
+			+ (testVec1.Length() == Mathf.Sqrt(20)
+			&& testVec1.GetAngleDegrees() == testDeg1
+			&& FloatIsEqual(testVec2.GetAngleDegrees(), -45)));
+		ResetTestValues();
+	}
+
+	private void RotateDegreesTest()
+	{
+		testVec1.SetAngleDegrees(30);
+		testVec1.RotateDegrees(50);
+		testVec2.SetAngleDegrees(120);
+		testVec2.RotateDegrees(-70);
+		Console.WriteLine("Rotate Degees ok? :"
+			+ (FloatIsEqual(testVec1.GetAngleDegrees(), 80)
+			&& FloatIsEqual(testVec2.GetAngleDegrees(), 50)));
+		ResetTestValues();
+	}
+
+	private void RotateRadiansTest()
+	{
+		testVec1.SetAngleRadians(0.3f * Mathf.PI);
+		testVec1.RotateRadians(0.2f * Mathf.PI);
+		testVec2.SetAngleRadians(0.6f * Mathf.PI);
+		testVec2.RotateRadians(-0.2f * Mathf.PI);
+		Console.WriteLine("Rotate Radians ok? :"
+			+ (FloatIsEqual(testVec1.GetAngleRadians(), 0.5f * Mathf.PI)
+			&& FloatIsEqual(testVec2.GetAngleRadians(), 0.4f * Mathf.PI)));
+		ResetTestValues();
+	}
+
+	private void RotateAroundDegreesTest()
+	{
+		testVec1.SetXY(1, 1);
+		testVec2.SetXY(2, 1);
+		testVec2.RotateAroundDegrees(testVec1, 90);
+		Console.WriteLine("Rotate Around Degrees ok? :" + (FloatIsEqual(testVec2.x, 1)
+			&& FloatIsEqual(testVec2.y, 2)));
+		ResetTestValues();
+	}
+
+	private void RotateAroundRadiansTest()
+	{
+		testVec1.SetXY(1, 1);
+		testVec2.SetXY(2, 1);
+		testVec2.RotateAroundRadians(testVec1, Mathf.PI);
+		Console.WriteLine("Rotate Around Radians ok? :"
+			+ (FloatIsEqual(testVec2.x, 0)
+			&& FloatIsEqual(testVec2.y, 1)));
+		ResetTestValues();
+	}
+
+	private void DotTest()
+	{
+		testVec1.SetXY(2, 2);
+		testVec2.SetXY(3, 3);
+
+		Console.WriteLine("Dot Product ok? :"
+			+ (testVec1.Dot(testVec2) == 12));
+		ResetTestValues();
+	}
+
+	private void UnitNormalTest()
+	{
+		testVec1.SetXY(5, 0);
+		Console.WriteLine("Unit Normal ok? :"
+			+ (FloatIsEqual(testVec1.UnitNormal().x, 0)
+			&& FloatIsEqual(testVec1.UnitNormal().y, 1)
+			&& FloatIsEqual(testVec1.UnitNormal().Length(), 1)));
+		ResetTestValues();
+	}
+
+	private void ReflectTest()
+	{
+		testVec1.SetAngleDegrees(45);
+		testVec2.SetAngleDegrees(0);
+		testVec1.Reflect(testVec2, 1);
+		Console.WriteLine("Reflect ok? :"
+			+ (FloatIsEqual(testVec1.GetAngleDegrees(), -45)));
+		ResetTestValues();
+	}
+}
+
