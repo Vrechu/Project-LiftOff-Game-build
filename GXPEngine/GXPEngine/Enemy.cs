45996e5ff4c463a9df95a542d166131a050996bd
Dylan Doornbos <482324@student.saxion.nl>
2021-02-09 12:53:37 +0100

    Added and implemented the Projectile class.

--- GXPEngine/GXPEngine/Enemy.cs
+++ GXPEngine/GXPEngine/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GXPEngine.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,17 @@ namespace GXPEngine
         private float moveSpeed = 2f; //The move speed of the enemy
         private float moveUntilDistance = 200f; //The enemy moves towards a target until this distance away
 
-        //The position in Vector2
+        private int chargeStartTime; //The last time the enemy started charging a projectile
+        private int chargeDuration = 500; //The time it takes to charge
+        private int projectileShotTime; //The last time the enemy shot a projectile
+        private int chargeCooldown = 1000; //The cooldown (after shooting) before the enemy can charge again
+        private bool isCharging; //Whether the enemy is currently charging
+
+        private Projectile projectileToCharge; //The projectile that's charging
+
+        private Transformable target; //The target the enemy is following
+
+        //The position in Vec2
         private Vec2 position
         {
             get
@@ -24,50 +35,95 @@ namespace GXPEngine
             }
         }
 
-        public Enemy(string enemySprite) : base(enemySprite)
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="spawnX">The X coordinates to spawn the enemy at</param>
+        /// <param name="spawnY">The Y coordinates tp spawn the enemy at</param>
+        /// <param name="newTarget">The target the enemy will follow and shoot at</param>
+        public Enemy(float spawnX, float spawnY, Transformable newTarget) : base("triangle.png")
         {
-            Initialize();
+            Initialize(spawnX, spawnY, newTarget);
         }
 
-        private void Initialize()
+        private void Initialize(float spawnX, float spawnY, Transformable newTarget)
         {
             SetOrigin(width / 2, height / 2); //Center the origin of the enemy
+            SetXY(spawnX, spawnY); //Set the X and Y position
+            target = newTarget; //Set the target
         }
 
         public void Update()
         {
-            RotateTowards();
-            MoveTowards();
+            MoveTowards(target);
+
+            //If the enemy is already charging
+            if (isCharging)
+                ChargeCurrentProjectile();
+            //Else if the charge cooldown is done
+            else if (Time.now >= projectileShotTime + chargeCooldown)
+                ChargeNewProjectile();
         }
 
         /// <summary>
-        /// Move towards the target
+        /// Move the enemy towards the designated target
         /// </summary>
-        private void MoveTowards(/*Transformable target*/)
+        /// <param name="moveTarget">The target to move towards</param>
+        private void MoveTowards(Transformable moveTarget)
         {
-            Vec2 target = new Vec2(Input.mouseX, Input.mouseY); //TODO: Placeholder target
+            Vec2 targetPosition = new Vec2(moveTarget.x, moveTarget.y); //Get the position of the target as a Vec2
 
-            float distanceToTarget = new Vec2(target.x - position.x, target.y - position.y).Length();
-
-            if (distanceToTarget > moveUntilDistance)
+            //If the distance to the target is large enough to still move
+            if (DistanceTo(moveTarget) > moveUntilDistance)
             {
-                Vec2 targetPosition = target - position;
-                targetPosition.Normalize();
+                Vec2 moveDirection = targetPosition - position; //Calculate the direction to move in
+                moveDirection.Normalize(); //Normalize the move direction
 
-                position += targetPosition * moveSpeed;
+                position += moveDirection * moveSpeed; //Move 'moveSpeed' units towards the move direction
             }
         }
 
         /// <summary>
-        /// Rotate towards the target
+        /// Start charging a new projectile
         /// </summary>
-        private void RotateTowards(/*Transformable target*/)
+        private void ChargeNewProjectile()
         {
-            Vec2 target = new Vec2(Input.mouseX, Input.mouseY); //TODO: Placeholder target
+            isCharging = true; //Set state to charging
 
-            target = target - position;
+            projectileToCharge = new Projectile(this); //Spawn a new projectile
 
-            rotation = target.GetAngleDegrees() + 90;
+            chargeStartTime = Time.now; //Remember at which time the enemy started charging
+
+            projectileToCharge.RotateTowards(target); //Rotate the projectile towards the target
+        }
+
+        /// <summary>
+        /// Continue charging a projectile
+        /// </summary>
+        private void ChargeCurrentProjectile()
+        {
+            projectileToCharge.RotateTowards(target); //Rotate the projectile towards the target
+
+            //If the enemy has charged for a sufficient amount of time
+            if (Time.now >= chargeStartTime + chargeDuration)
+                ShootProjectileTowards(target); //Shoot the projectile
+        }
+
+        /// <summary>
+        /// Shoot the charging projectile towards the target
+        /// </summary>
+        /// <param name="shootTarget">The target to shoot the projectile at</param>
+        private void ShootProjectileTowards(Transformable shootTarget)
+        {
+            isCharging = false; //Set state to not charging
+
+            projectileShotTime = Time.now; //Remember the time the projectile was shot at
+
+            Vec2 targetLocation = new Vec2(shootTarget.x, shootTarget.y); //Get the location of the target as a Vec2
+
+            Vec2 targetRotation = targetLocation - position; //Get the rotation to shoot the projectile in
+
+            projectileToCharge.Shoot(targetRotation); //Shoot the projectile in the target's direction
         }
     }
 }
---
+++ GXPEngine/GXPEngine/Projectile.cs
@@ -0,0 +1,88 @@
+﻿using GXPEngine.Core;
+using GXPEngine.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace GXPEngine
+{
+    class Projectile : Sprite
+    {
+        private float moveSpeed = 5f; //The speed at which the projectile moves
+        private float distanceFromSource = 20f; //The distance from its source
+
+        private Vec2 moveDirection; //The direction to move in
+        private bool isFired = false; //Whether the projectile has been fired
+        private GameObject source; //The source object that the projectile came from
+
+        //The position in Vector2
+        private Vec2 position
+        {
+            get
+            {
+                return new Vec2(x, y);
+            }
+            set
+            {
+                x = value.x;
+                y = value.y;
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="newSource">The source object that the projectile came from</param>
+        public Projectile(GameObject newSource) : base("circle.png")
+        {
+            Initialize(newSource);
+        }
+
+        private void Initialize(GameObject newSource)
+        {
+            SetOrigin(width / 2, height + distanceFromSource); //Set the origin
+            source = newSource; //Set the source
+            scale = 0.5f; //Set the scale
+            SetXY(source.x, source.y); //Set the
+            game.AddChild(this);
+        }
+
+        void Update()
+        {
+            //If the projectile is fired
+            if (isFired)
+                position += moveDirection * moveSpeed; //Move in the fired direction
+            else
+            {
+                SetXY(source.x, source.y); //Otherwise follow the position of the source
+            }
+        }
+
+        /// <summary>
+        /// Shoot the projectile
+        /// </summary>
+        /// <param name="launchDirection">The direction to shoot the projectile in</param>
+        public void Shoot(Vec2 launchDirection)
+        {
+            SetOrigin(width / 2, height / 2); //Center the origin
+
+            moveDirection = launchDirection; //Set the direction the bullet is shot at
+            moveDirection.Normalize(); //Normalize the direction
+
+            isFired = true; //Set state to having been fired
+        }
+
+        /// <summary>
+        /// Rotate the projectile towards the designated target
+        /// </summary>
+        /// <param name="rotationTarget">The target to rotate towards</param>
+        public void RotateTowards(Transformable rotationTarget)
+        {
+            Vec2 targetPosition = new Vec2(rotationTarget.x, rotationTarget.y); //Get the position of the target as a Vec2
+            Vec2 targetRotation = targetPosition - position; //Get the rotation towards the target
+            rotation = targetRotation.GetAngleDegrees() + 90; //Set the rotation of the projectile
+        }
+    }
+}
