ea0c37a7aa42edfe1dbb3d94800eea766eddfd08
Dylan Doornbos <482324@student.saxion.nl>
2021-02-10 13:18:20 +0100

    Added a line of sight.
    Added a line of sight class.
    Reduce the chance of enemies shooting each other.

--- GXPEngine/GXPEngine/Enemy.cs
+++ GXPEngine/GXPEngine/Enemy.cs
@@ -6,10 +6,10 @@ using System.Text;
 
 namespace GXPEngine
 {
-    class Enemy : Sprite
+    class Enemy : AnimationSprite
     {
         private float moveSpeed = 2f; //The move speed of the enemy
-        private float moveUntilDistance = 400f; //The enemy moves towards a target until they are this distance away
+        private float distanceFromTarget = 400f; //The enemy moves towards a target until they are this distance away
 
         private int projectileShotTime; //The last time the enemy shot a projectile
         private int shotCooldown = 1500; //The cooldown (after shooting) before the enemy can shoot again
@@ -17,6 +17,7 @@ namespace GXPEngine
         private Projectile projectileToCharge; //The projectile that's charging
 
         private Transformable target; //The target the enemy is following
+        private LineOfSight lineOfSight;
 
         //The position in Vec2
         private Vec2 Position
@@ -38,7 +39,7 @@ namespace GXPEngine
         /// <param name="spawnX">The X coordinates to spawn the enemy at</param>
         /// <param name="spawnY">The Y coordinates tp spawn the enemy at</param>
         /// <param name="newTarget">The target the enemy will follow and shoot at</param>
-        public Enemy(float spawnX, float spawnY, Transformable newTarget) : base("triangle.png")
+        public Enemy(float spawnX, float spawnY, Transformable newTarget) : base("FrogodileGrid.png", 5, 4, 18)
         {
             Initialize(spawnX, spawnY, newTarget);
         }
@@ -48,15 +49,33 @@ namespace GXPEngine
             SetOrigin(width / 2, height / 2); //Center the origin of the enemy
             SetXY(spawnX, spawnY); //Set the X and Y position
             target = newTarget; //Set the target
+
+            //Create a new line of sight
+            lineOfSight = new LineOfSight(this);
+            AddChild(lineOfSight);
+
             name = "Enemy";
         }
 
         public void Update()
         {
-            MoveTowards(target); //Move towards the target
+            lineOfSight.SetRotation(RotationTowards(target)); //Rotate the line of sight towards the target
+            lineOfSight.SetLength(DistanceTo(target)); //Set the length of the line of sight to match the distance between the enemy and target
+
+            //If the line of sight is colliding with an enemy that's not its source
+            if(lineOfSight.CollidingWithEnemy)
+            {
+                RotateAround(target); //Rotate around the target
+            }
+
+            //If the distance to the target is large enough to still move
+            if (DistanceTo(target) > distanceFromTarget)
+            {
+                MoveInDirection(RotationTowards(target)); //Move towards the target
+            }
 
             //If the cooldown has run out
-            if (Time.now > projectileShotTime + shotCooldown)
+            if (Time.now > projectileShotTime + shotCooldown && !lineOfSight.CollidingWithEnemy)
                 ChargeNewProjectile(); //Charge a new projectile
         }
 
@@ -71,31 +90,53 @@ namespace GXPEngine
         /// Move the enemy towards the designated target
         /// </summary>
         /// <param name="moveTarget">The target to move towards</param>
-        private void MoveTowards(Transformable moveTarget)
+        private Vec2 RotationTowards(Transformable moveTarget)
         {
             Vec2 targetPosition = new Vec2(moveTarget.x, moveTarget.y); //Get the position of the target as a Vec2
 
-            //If the distance to the target is large enough to still move
-            if (DistanceTo(moveTarget) > moveUntilDistance)
-            {
-                Vec2 moveDirection = targetPosition - Position; //Calculate the direction to move in
-                moveDirection.Normalize(); //Normalize the move direction
+            Vec2 moveDirection = targetPosition - Position; //Calculate the direction to move in
+            moveDirection.Normalize(); //Normalize the move direction
 
-                Position += moveDirection * moveSpeed; //Move 'moveSpeed' units towards the move direction
-            }
+            return moveDirection;
+        }
+
+        /// <summary>
+        /// Rotate around an object
+        /// </summary>
+        /// <param name="rotationTarget">The object to rotate around</param>
+        private void RotateAround(Transformable rotationTarget)
+        {
+            Vec2 newRotation = RotationTowards(rotationTarget);
+            newRotation.RotateDegrees(90);
+            MoveInDirection(newRotation);
+        }
+
+        /// <summary>
+        /// Move in the designated direction
+        /// </summary>
+        /// <param name="moveDirection">The direction to move in</param>
+        private void MoveInDirection(Vec2 moveDirection)
+        {
+            moveDirection.Normalize();
+            Position += moveDirection * moveSpeed; //Move 'moveSpeed' units towards the move direction
         }
 
         public void OnCollision(GameObject other)
         {
-            if(other is Projectile)
+            if (other is Projectile)
             {
                 Projectile projectile = other as Projectile;
                 if (projectile.HasLeftSource)
                 {
                     LateDestroy();
                     projectile.LateDestroy();
-                    projectileToCharge.LateDestroy();
                 }
+            }
+            else if(other is Enemy)
+            {
+                Vec2 newRotation = RotationTowards(other);
+                newRotation.RotateDegrees(180);
+                MoveInDirection(newRotation);
             }
         }
     }
---
+++ GXPEngine/GXPEngine/LineOfSight.cs
@@ -0,0 +1,62 @@
+﻿using GXPEngine.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    class LineOfSight : Sprite
+    {
+        public bool CollidingWithEnemy { get; private set; } = true; //Whether the line of sight is colliding with another enemy
+        private Transformable source; //The source of the line of sight
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="newSource">The source object of the line of sight</param>
+        public LineOfSight(Transformable newSource) : base("square.png")
+        {
+            Initialize(newSource);
+        }
+
+        private void Initialize(Transformable newSource)
+        {
+            SetOrigin(width / 2, height);
+            alpha = 0;
+
+            source = newSource;
+        }
+
+        public void Update()
+        {
+            CollidingWithEnemy = false;
+        }
+
+        /// <summary>
+        /// Set the rotation of the line of sight
+        /// </summary>
+        /// <param name="targetRotation">The rotation to set it at</param>
+        public void SetRotation(Vec2 targetRotation)
+        {
+            rotation = targetRotation.GetAngleDegrees() + 90;
+        }
+
+        /// <summary>
+        /// Set the length of the line of sight
+        /// </summary>
+        /// <param name="newLength">The new length of the line of sight</param>
+        public void SetLength(float newLength)
+        {
+            height = (int)newLength;
+        }
+
+        public void OnCollision(GameObject other)
+        {
+            if(other is Enemy && other != source)
+            {
+                CollidingWithEnemy = true;
+            }
+        }
+    }
+}
