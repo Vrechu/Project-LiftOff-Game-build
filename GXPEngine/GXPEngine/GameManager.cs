5b7d24833c7414a6df53b62e84d9d64824432a30
Freek Michielsen <480709@student.saxion.nl>
2021-02-10 11:03:54 +0100

    Add game manager

---
+++ GXPEngine/GXPEngine/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GXPEngine;
+
+class GameManager
+{
+    private static GameManager singleton;
+
+    public static GameManager Singleton
+    {
+        get
+        {
+            if (singleton == null)
+            {
+                singleton = new GameManager();
+            }
+            return singleton;
+        }
+    }
+
+    private GameManager() { }
+
+    private int _playerHealth;
+
+    //
+    public void PlayerGetsHit(int damage)
+    {
+        _playerHealth -= damage;
+        _playerHealth = Mathf.Max(0, _playerHealth);
+    }
+}
+
