1cf01c10c69802ef158d4ec8a346b4936a568706
Freek Michielsen <480709@student.saxion.nl>
2021-02-11 14:55:09 +0100

    Button
    Button

---
+++ GXPEngine/GXPEngine/Button.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GXPEngine;
+
+class Button : Sprite
+{
+    public Button(float px, float py) : base("square.png")
+    {
+        SetOrigin(width / 2, height / 2);
+        SetScaleXY(0.5f, 0.5f);
+        x = px;
+        y = py;
+    }
+}
+
