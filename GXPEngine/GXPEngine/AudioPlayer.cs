21dc3489b0239c1dc0f69403374bde71ef969c67
Freek Michielsen <480709@student.saxion.nl>
2021-02-17 14:30:03 +0100

    Add sound and better spawnpoints

---
+++ GXPEngine/GXPEngine/AudioPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using GXPEngine.Projectiles;
+using System.Linq;
+using System.Text;
+using GXPEngine;
+
+class AudioPlayer : GameObject
+{
+    MyGame _myGame;
+       public AudioPlayer(MyGame myGame)
+    {
+        _myGame = myGame;
+        EventSubscriptions();
+    }      
+    
+    private void EventSubscriptions()
+    {
+        Player.OnPLayerHit += PlayHurtSound;
+        Projectile.OnShot += PlayProjectileShotSound;
+    }
+
+    void OnDestroy()
+    {
+        Player.OnPLayerHit -= PlayHurtSound;
+        Projectile.OnShot -= PlayProjectileShotSound;
+    }
+
+    private void PlayHurtSound()
+    {
+        new Sound("character_hit.mp3").Play(); // ...play a sound
+    }
+
+    private void PlayDeathSound()
+    {
+
+    }
+
+    private void PlayReflectSound()
+    {
+        
+    }
+
+    private void PlayProjectileShotSound(ProjectileType projectileType)
+    {
+
+    }
+}
+
